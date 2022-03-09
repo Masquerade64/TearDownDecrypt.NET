225b4b95b58b84eadb872070be525b7a25b0dea9
Masquerade <87361986+Masquerade64@users.noreply.github.com>
2022-03-09 19:36:05 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System;
+
+namespace TearDownDecrypt.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string a = "599Cc51887A8cb0C20F9CdE34cf9e0A535E5cAd1C26c7b562596ACC207Ae9A0bfB3E3161f31af5bEf1c2f064b3628174D83BF6E0739D9D6918fD9C2Eba02D5aD";
+                string b = "0C3b676fe8d7188Cde022F71632830F36b98b181aD48Fed160006eA59";
+
+                using (FileStream fin = new FileStream(args[0], FileMode.Open), fout = new FileStream(args[1], FileMode.Create))
+                {
+                    int dat = 0;
+                    int i = 0;
+                    while ((dat = fin.ReadByte()) != -1)
+                    {
+                        var d = (byte)dat ^ (byte)a[i % 128] ^ (byte)b[i % 57];
+                        fout.WriteByte((byte)d);
+                        i++;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
